a31ee40ec60d1cb03a9742b308d1dfed8744ab3c
EdsonCampolina <ejcsengsoft@gmail.com>
2022-03-06 14:36:37 -0300

    Adicionando Facade

---
+++ ProjetoFacadeDIOBootcamp/src/one/digitalinnovation/Teste.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Facade facade = new Facade();
+		facade.migrarCliente("Edson", "1231231231");
+
+	}
+
+}
